2d84d43196a2f9bf031cffc5db1bb5b65bf907ad
Mks <mukundsrinivasan0@gmail.com>
2024-02-18 19:53:28 +1100

    accidentally committing all of p5js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-let canvas = document.getElementById("myCanvas");
-let context = canvas.getContext("2d");
-
-context.beginPath();
-context.rect(200, 200, 100, 100);
-context.fill();
-context.closePath();
--- src/testing.js
+++ src/testing.js
@@ -2,3 +2,11 @@ function Add(number1, number2) {
     return number1 + number2;
 }
 console.log("Mukund says hi! Look what this is, adding 2 and 3 = " + Add(2, 3));
+
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+
+context.beginPath();
+context.rect(200, 200, 100, 100);
+context.fill();
+context.closePath();
