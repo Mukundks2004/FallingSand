702891f0e69dd8e81f06ecd687b7b92dc7af282e
Mks <mukundsrinivasan0@gmail.com>
2024-03-10 18:35:49 +1100

    deleted test code deleted superfluous css and html decreased blob size radius fixed bug that made sand change colour as it fell organized sketch.js and added comments separated README into description and markdown tutorial index.html only has sketch and imports

---
+++ MarkdownTutorial.md
@@ -0,0 +1,94 @@
+# Heading
+
+I am going to practice typing so i can get a feel for markdown
+
+## this is a smaller heading
+
+### this is a tinier heading, it has 3 hashes
+
+* this is a list point
+* this is another list point
+    * ooh what happends now??
+    * indented element
+
+1. this is ordered
+1. number two
+1. number three
+    1. indented
+1. back a line, but with a space
+
+Ready for the next section?
+
+\\ backslash escapes characters such as \*
+
+otherwise \* cause *italics* and if you do them twice you get **BOLD**
+same goes for \_ underscores
+
+~~you can do strikethrough with tildes but pretend i did not just tell you that~~
+
+# Another heading before we move on
+---
+this is sandwiched between lines
+
+___
+
+
+[Here is a link to google](https://google.com.au "This is a link to google but the title!")
+
+
+<!-- This is a comment that you dont get to see! -->
+
+# Now for the real fun:
+
+__it's time for some github specific stuff!__
+
+I wonder if theres an extension cuz i would loove some latex in here
+
+```python
+
+def my_private_func(my_paramter):
+    # do some stuff woah it's green
+    print("Mukund is so cool!")
+    return my_parameter ** 2
+```
+
+> Mukund, 2024
+
+*the above line is a quote*
+
+# experimenting below
+
+$$\int_{-\infty}^{\infty}e^{-{x^2}}dx = \sqrt{\pi}$$
+
+doubt that will work lol
+
+---
+
+```
+generic code here
+
+@ echo off 
+
+```
+___
+apparently the above also does a line, it's a triple underscore
+
+`<p>here is a html paragraph i produced using backticks</p>`
+
+wow aint that neat!
+
+```csharp
+//This is some C# code
+
+class Program {
+
+    static void Main(string[] args) {
+        Console.WriteLine("Hello world!");
+    }
+
+    public static int Square(int number) {
+        return number * number;
+    }
+    
+}
+```
--- sketch.js
+++ sketch.js
@@ -1,22 +1,9 @@
 let rows, cols;
-let squareSize = 2;
-let blobSize = 5;
-let array, colourGrid, movingGrid;
-//the array grid contains the zeroes and ones that correspond to where sand is located and where it is not
-//the moving grid contains a 
-let mukundBlue;
+let squareSize = 5;
+let blobSize = 3;
+let array, colourGrid;
 
-function makeZeroArray() {
-  let arr = new Array(rows);
-  for (let i = 0; i < rows; i++) {
-    arr[i] = new Array(cols);
-    for (let j = 0; j < cols; j++) {
-      arr[i][j] = 0;
-    }
-  }
-  return arr;
-}
-
+//Used to keep all sand on the screen
 function isRowInBounds(row) {
   return row >= 0 && row < rows;
 }
@@ -25,34 +12,42 @@ function isColInBounds(col) {
   return col >= 0 && col < cols;
 }
 
-function colourMyGrid(myGrid, myColour) {
-  for (let i = 0; i < myGrid.length; i++) {
-    for (let j = 0; j < myGrid.length; j++) {
-      myGrid[i][j] = varyBlue(myColour);
+//Initializes grid with 0s
+function makeZeroArray() {
+  let sandGrid = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    sandGrid[i] = new Array(cols);
+    for (let j = 0; j < cols; j++) {
+      sandGrid[i][j] = 0;
     }
   }
+  return sandGrid;
 }
 
-function setup() {
-  createCanvas(600, 600);
-  cols = width / squareSize;
-  rows = height / squareSize;
-  array = makeZeroArray();
-  colourGrid = makeZeroArray();
-  movingGrid = makeZeroArray();
-  mukundBlue = color("orange");
-  colourMyGrid(colourGrid, mukundBlue);
-}
-
-function varyBlue(color) {
+//Varies colour by a little
+function varyColour(color) {
   let Hue = floor(hue(color)) + floor(random(-0, 0));
   let Sat = saturation(color) + floor(random(0, 10));
   let Light = lightness(color) + floor(random(-10, 0));
   return `hsl(${Hue}, ${Sat}%, ${Light}%)`;
 }
 
+function setup() {
+  createCanvas(300, 300);
+  cols = width / squareSize;
+  rows = height / squareSize;
+  array = makeZeroArray();
+  colourGrid = makeZeroArray();
+  movingGrid = makeZeroArray();
+  sandBaseColour = color("brown");
+}
+
 function draw() {
+
+  //Reset canvas
   background(0);
+
+  //Generate sand
   if (mouseIsPressed) {
     let mouseCol = floor(mouseX / squareSize);
     let mouseRow = floor(mouseY / squareSize);
@@ -60,12 +55,13 @@ function draw() {
       for (let j = -blobSize; j <= blobSize; j++) {
         if (isRowInBounds(mouseRow + i) && isColInBounds(mouseCol + j) && Math.random() > 0.6) {
           array[mouseRow + i][mouseCol + j] = 1;
+          colourGrid[mouseRow + i][mouseCol + j] = varyColour(color("brown"));
         }
       }
     }
   }
 
-  
+  //Draw sand
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       noStroke();
@@ -80,22 +76,31 @@ function draw() {
     }
   }
 
+  //Drop sand
   for (let row = rows - 2; row >= 0; row--) {
     for (let col = 0; col < cols; col++) {
       if (array[row][col] == 1) {
         if (array[row + 1][col] == 0) {
           array[row + 1][col] = 1;
           array[row][col] = 0;
+          colourGrid[row + 1][col] = colourGrid[row][col];
+          colourGrid[row][col] = 0;
         }
+
+        //Spill if sand cannot drop directly down
         else {
           direction = Math.random() > 0.5 ? 1 : -1;
           if (array[row + 1][col + direction] == 0) {
             array[row + 1][col + direction] = 1;
             array[row][col] = 0;
+            colourGrid[row + 1][col + direction] = colourGrid[row][col];
+            colourGrid[row][col] = 0;
           }
           else if (array[row + 1][col - direction] == 0) {
             array[row + 1][col - direction] = 1;
             array[row][col] = 0;
+            colourGrid[row + 1][col - direction] = colourGrid[row][col];
+            colourGrid[row][col] = 0;
           }
         }
       }
